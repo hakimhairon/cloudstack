cfd1afba2c2e2fd45386c84f96bf3e23ebd9996f
hakimhairon <69628278+hakimhairon@users.noreply.github.com>
2021-01-22 19:06:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,21 +6,6 @@
 <body>
 
 <h1>Hi Hakim</h1>
-
-<?php
-$servername = "hakimdb.cedtk3g7pkh0.us-east-1.rds.amazonaws.com";
-$username = "admin";
-$password = "admin123";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
   
 </body>
 </html>
