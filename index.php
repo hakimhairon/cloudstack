577baeab14642257aa68bee5429af41b3ff9c66d
hakimhairon <69628278+hakimhairon@users.noreply.github.com>
2021-01-22 18:40:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>Hi Hakim</h1>
+
 <?php
 $servername = "hakimdb.cedtk3g7pkh0.us-east-1.rds.amazonaws.com";
 $username = "admin";
@@ -12,3 +21,6 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+  
+</body>
+</html>
