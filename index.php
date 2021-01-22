a5f6642b1e45dd03fcb34692296c3db9655327d7
hakimhairon <69628278+hakimhairon@users.noreply.github.com>
2021-01-22 18:29:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>hi hakimhairon</h1>
-
-
 <?php
-$servername = "localhost";
+$servername = "hakimdb.cedtk3g7pkh0.us-east-1.rds.amazonaws.com";
 $username = "admin";
-$password = "admin";
-$database = "hakim";
+$password = "admin123";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * from info";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Name: " . $row["nama"]. "<br>";
-    echo "No: " . $row["no"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+echo "Connected successfully";
 ?>
-
-</body>
-</html>
-
